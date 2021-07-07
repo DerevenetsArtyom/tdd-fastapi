bdfefee299f813e3dc3f74a40636e45e9dd87dd9
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-07-07 21:47:22 +0300

    Add settings

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+from pydantic import BaseSettings
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+
+
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
+
--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from app.config import get_settings, Settings
 
 app = FastAPI()
 
 
 @app.get("/ping")
-def pong():
-    return {"ping": "pong!"}
+def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
+
