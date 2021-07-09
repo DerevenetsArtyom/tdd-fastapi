82df5a8c73b7318e44a7815e54f266d4efed79cf
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-07-09 21:55:25 +0300

    Register models via 'register_tortoise'

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,19 @@
+import os
+
 from fastapi import FastAPI, Depends
 
 from app.config import get_settings, Settings
+from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.getenv("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
@@ -12,4 +23,3 @@ async def pong(settings: Settings = Depends(get_settings)):
         "environment": settings.environment,
         "testing": settings.testing
     }
-
