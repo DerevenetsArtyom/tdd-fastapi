51da44a93efee55f0e1e82e1bdeb0be25cf9af71
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-07-11 14:31:42 +0300

    Add 'test_create_summary_invalid_json'

--- src/tests/test_summaries.py
+++ src/tests/test_summaries.py
@@ -6,3 +6,12 @@ def test_create_summary(test_app_with_db):
 
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar"
+
+
+def test_create_summary_invalid_json(test_app):
+    response = test_app.post("/summaries/", data=json.dumps({}))
+
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [{"loc": ["body", "url"], "msg": "field required", "type": "value_error.missing"}]
+    }
