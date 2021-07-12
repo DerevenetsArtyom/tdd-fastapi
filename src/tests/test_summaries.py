30e0df5207ad4363fee3bd9e48761e52939e8d67
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-07-12 09:52:36 +0300

    Create 'test_read_summary' test

--- src/tests/test_summaries.py
+++ src/tests/test_summaries.py
@@ -15,3 +15,18 @@ def test_create_summary_invalid_json(test_app):
     assert response.json() == {
         "detail": [{"loc": ["body", "url"], "msg": "field required", "type": "value_error.missing"}]
     }
+
+
+def test_read_summary(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.get(f"/summaries/{summary_id}")
+    assert response.status_code == 200
+
+    response_dict = response.json()
+
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"]
+    assert response_dict["created_at"]
