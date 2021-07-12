b8ca409388c1809644095d9310f38665409f7199
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-07-12 09:58:32 +0300

    Add 'test_read_summary_wrong_id' and application code for it

--- src/app/api/summaries.py
+++ src/app/api/summaries.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
@@ -18,5 +18,7 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
 @router.get("/{summary_id}/", response_model=SummarySchema)
 async def read_summary(summary_id: int) -> SummarySchema:
     summary = await crud.get(summary_id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
 
     return summary
--- src/tests/test_summaries.py
+++ src/tests/test_summaries.py
@@ -30,3 +30,10 @@ def test_read_summary(test_app_with_db):
     assert response_dict["url"] == "https://foo.bar"
     assert response_dict["summary"]
     assert response_dict["created_at"]
+
+
+def test_read_summary_wrong_id(test_app_with_db):
+    response = test_app_with_db.get("/summaries/999")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
